146888afb84d175c94e3b4a05f80044896d6bf2c
songhn233 <songhn233@gmail.com>
2021-10-04 13:55:30 +0800

    style: :art: refactor 404 page loading styles

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -9,7 +9,7 @@ onMounted(() => {
     () => {
       router.push('/')
     },
-    3000,
+    2000,
   ) as unknown as number
 })
 onUnmounted(() => {
@@ -18,7 +18,10 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="text-xl opacity-80 text-gray-600 dark:text-light-600">
+  <div class="text-xl opacity-80 text-gray-600 dark:text-light-600 w-full flex flex-col items-center">
+    <div class="lds-ripple mb-3">
+      <div></div><div></div>
+    </div>
     {{ t('not-found') }}
   </div>
 </template>
