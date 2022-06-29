26fbea7ce8af9ace3195df20182b70610a5b969b
songhn233 <songhn233@gmail.com>
2022-06-30 00:20:21 +0800

    fix: global window interface

--- hooks/useDark.tsx
+++ hooks/useDark.tsx
@@ -1,3 +1,11 @@
+// extrends for dark mode global function
+declare global {
+  interface Window {
+    __theme: string;
+    __setTheme: (theme: string) => void;
+  }
+}
+
 export function useDark() {
   const toggleDark = () => {
     window.__setTheme(window.__theme === 'light' ? 'halloween' : 'light');
--- next-env.d.ts
+++ next-env.d.ts
@@ -3,9 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-// extrends for dark mode global function
-interface Window {
-  __theme: string;
-  __setTheme: (theme: string) => void;
-}
